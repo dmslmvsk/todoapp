1f6cd47951bb8351fd706290a2d1442190b73100
dmslmvsk <dmslmvsk@gmail.com>
2025-02-15 22:19:40 +0100

    Project init & Configuration

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/",(req,res) => {
+	res.send("hello world");
+})
+
+app.listen(port,() => {
+	console.log(`app is running on port ${port}`)
+})
